13d8fb26d75ef02417ae9c108b5ffd2140f58290
mortenlokkemoen <mortenlokkemoen@gmaile.com>
2022-12-01 23:28:25 +0100

    updated the index file since it was on a seperate branch

--- foodie-app/src/index.js
+++ foodie-app/src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux';
+import store from './reducers/configureStore'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <Router>
       <App />
     </Router>
+    </Provider>
   </React.StrictMode>
 );
 
